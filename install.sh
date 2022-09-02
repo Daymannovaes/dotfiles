286e0a19fe1390439b12ca5cd6cd27d183893122
Dayman Novaes <daymannovaes@gmail.com>
2022-09-01 20:03:50 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,6 +6,10 @@
 # installs oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+# install asdf
+brew install asdf
+
+
 
 DOTPATH=${DOTPATH:-~/dotfiles}
 REMOTE=${REMOTE:-https://github.com/Daymannovaes/dotfiles.git}
